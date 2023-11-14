04ca1b999627da6b8252f1056f6af1f44e82b037
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-11-14 18:58:52 -0300

    Initial project

---
+++ src/main/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplication.java
@@ -0,0 +1,9 @@
+package br.com.desnecesauron.javaunittestscourse;
+
+public class JavaUnitTestsCourseApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.desnecesauron.javaunittestscourse;
+
+import org.junit.jupiter.api.Test;
+
+class JavaUnitTestsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
