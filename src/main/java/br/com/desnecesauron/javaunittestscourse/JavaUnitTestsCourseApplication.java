a8f5e0d66f57081b1b1811d5ac6091a4c70d98c6
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-11-14 19:45:20 -0300

    14. 0304 Criando o primeiro teste unitário

--- src/main/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplication.java
+++ src/main/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplication.java
@@ -3,7 +3,7 @@ package br.com.desnecesauron.javaunittestscourse;
 public class JavaUnitTestsCourseApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("Hello World!");
 	}
 
 }
--- src/test/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplicationTests.java
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/JavaUnitTestsCourseApplicationTests.java
@@ -2,10 +2,17 @@ package br.com.desnecesauron.javaunittestscourse;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class JavaUnitTestsCourseApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
 	}
 
 }
