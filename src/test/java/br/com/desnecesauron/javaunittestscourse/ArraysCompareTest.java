2d88267a432f2cce193cbc89edf3ae7048b25f72
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-12-05 20:40:43 -0300

    31. 0321 Testando Performance em JUnit Tests

--- src/test/java/br/com/desnecesauron/javaunittestscourse/ArraysCompareTest.java
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/ArraysCompareTest.java
@@ -2,8 +2,10 @@ package br.com.desnecesauron.javaunittestscourse;
 
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -15,5 +17,17 @@ public class ArraysCompareTest {
         int[] expected = {1, 3, 4, 12};
         Arrays.sort(numbers);
         assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+//    @Timeout(1)
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int[] array = {12, 23, 4};
+        for (int i = 0; i < 1000000000; i++) {
+            array[0] = i;
+            Arrays.sort(array);
+        }
+
     }
 }
