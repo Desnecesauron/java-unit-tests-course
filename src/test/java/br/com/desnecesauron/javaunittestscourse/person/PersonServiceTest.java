ca76327fe6f3e3c7eb78c11046e6515d14db329d
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-12-11 20:14:58 -0300

    46. 0503 Criando o Teste de Criação de Persons

---
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/person/PersonServiceTest.java
@@ -0,0 +1,18 @@
+package br.com.desnecesauron.javaunittestscourse.person;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+    @DisplayName("When create a person with success should return the object of the created person")
+    @Test
+    void testCreatePerson_WhenCreateAPersonWithSuccess_ShouldReturnTheObjectOfTheCreatedPerson() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
