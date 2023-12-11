51cedcb107299b4924eec938a6e19602bdca9389
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-12-11 20:37:10 -0300

    49. 0506 Verificando se a Pessoa Criada Contém um FirstName

--- src/main/java/br/com/desnecesauron/javaunittestscourse/person/PersonService.java
+++ src/main/java/br/com/desnecesauron/javaunittestscourse/person/PersonService.java
@@ -3,6 +3,6 @@ package br.com.desnecesauron.javaunittestscourse.person;
 public class PersonService implements IPersonService {
     @Override
     public Person createPerson(Person person) {
-        return new Person();
+        return person;
     }
 }
--- src/test/java/br/com/desnecesauron/javaunittestscourse/person/PersonServiceTest.java
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/person/PersonServiceTest.java
@@ -1,9 +1,9 @@
 package br.com.desnecesauron.javaunittestscourse.person;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PersonServiceTest {
@@ -19,6 +19,20 @@ public class PersonServiceTest {
         Person actual = personService.createPerson(person);
         // then
         assertNotNull(actual, () -> "The created person is null!");
+    }
+
+    @DisplayName("When create a person with success should should contain first name in returned object")
+    @Test
+    void testCreatedPerson_WhenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
+        // given
+        IPersonService personService = new PersonService();
+
+        Person person = new Person("Keith", "Moon", "kmoon@gmail.com", "Wembley - UK", "Male");
+        // when
+        Person actual = personService.createPerson(person);
+        // then
+        assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The first name is not the same!");
+
     }
 
 }
