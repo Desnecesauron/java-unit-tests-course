86f54e5ab82d8a192cd74fe8de1318e20956735d
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2023-12-13 20:11:34 -0300

    63. 0607 Setando os Valores a serem Retornados pelo Mock

--- src/test/java/br/com/desnecesauron/javaunittestscourse/mockito/service/CourseBusinessMockTest.java
+++ src/test/java/br/com/desnecesauron/javaunittestscourse/mockito/service/CourseBusinessMockTest.java
@@ -3,25 +3,32 @@ package br.com.desnecesauron.javaunittestscourse.mockito.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CourseBusinessMockTest {
 
     CourseService mockService;
     CourseBusiness business;
+    List<String> listCourses;
 
     @BeforeEach
     void setUp() {
         // Given / Arrange
         mockService = mock(CourseService.class);
         business = new CourseBusiness(mockService);
+        listCourses = Arrays.asList("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
+                "Kubernetes");
     }
 
     @Test
     void testCoursesRelatedToSpring_When_UsingAMock() {
+        // Given / Arrange
+        when(mockService.retrieveCourses("Cesar")).thenReturn(listCourses);
         // When / Act
         List<String> stringList = business.retrieveCoursesRelatedToSpring("Cesar");
         // Then / Assert
